b343d17264f8f759fb002bc6a8d3f4256c6db0a8
user42 <m4xth9r@gmail.com>
2020-10-14 17:39:15 +0200

    straight forward

--- list/list.hpp
+++ list/list.hpp
@@ -4,72 +4,71 @@
 namespace ft
 {
 
+	template<typename T>
+	class Node
+	{
+		public:
+			T c;
+			Node *prev;
+			Node *next;
+
+			Node() {}
+			Node(const T &content) : c(content), next(NULL), prev(NULL) {}
+	};
+
+	template<typename T>
+	class listIterator
+	{
+		private:
+			Node<T>* n;
+
+		public:
+
+			listIterator() {}
+			listIterator(Node<T> *current) : n(current) {}
+			listIterator(listIterator const *copy) : n(copy->n) {}
+			~listIterator() {}
+
+			listIterator& operator++()
+			{
+				n = n->next;
+				return (*this);
+			}
+
+			listIterator operator++(int)
+			{
+				Node<T>* temp = n;
+				n = n->next;
+				return (temp);
+			}
+
+			listIterator& operator--()
+			{
+				n = n->prev;
+				return (*this);
+			}
+
+			listIterator operator--(int)
+			{
+				Node<T>* temp = n;
+				n = n->prev;
+				return (temp);
+			}
+
+			bool operator!=(listIterator<T> const &rhs)
+			{
+				return (this->n != rhs.n);
+			}
+
+			T& operator*()
+			{
+				return (n->c);
+			}
+	};
+
 	template<class T, class Alloc = std::allocator<T> >
 	class list
 	{
-		private:
-
-			class Node
-			{
-				public:
-					T c;
-					Node *prev;
-					Node *next;
-
-					Node() {}
-					Node(const T &content) : c(content), next(NULL), prev(NULL) {}
-			};
-
-			typedef struct	s_list
-			{
-				Node *first;
-				Node *last;
-			}				t_list;
-
-			class listIterator
-			{
-				private:
-					Node* n;
-
-				public:
-
-					listIterator() {}
-					listIterator(Node *current) : n(current) {}
-					listIterator(listIterator *copy) : n(copy->n) {}
-					~listIterator() {}
-
-					listIterator& operator++()
-					{
-						n = n->next;
-						return (*this);
-					}
-
-					listIterator operator++(int)
-					{
-						Node* temp = n;
-						n = n->next;
-						return (temp);
-					}
-
-					listIterator& operator--()
-					{
-						n = n->prev;
-						return (*this);
-					}
-
-					listIterator operator--(int)
-					{
-						Node* temp = n;
-						n = n->prev;
-						return (temp);
-					}
-
-					T& operator*()
-					{
-						return (n->c);
-					}
-			};
-
 		public:
 			typedef T value_type;
 			typedef T *pointer;
@@ -80,48 +79,48 @@ namespace ft
 			typedef Alloc allocator_type;
 			typedef std::ptrdiff_t difference_type;
 
-			typedef listIterator iterator;
+			typedef listIterator<T> iterator;
+			typedef listIterator<const T> const_iterator;
 
 			typedef size_t size_type;
 
 		private:
+			typedef struct	s_list
+			{
+				Node<T> *first;
+				Node<T> *last;
+			}				t_list;
+
 			t_list _list;
 			allocator_type _allocator;
 			size_type _length;
-			size_type _capacity;
-
 
 			void init()
 			{
-				_list.first = NULL;
 				_list.last = NULL;
+				_list.first = NULL;
 			}
 
 		public:
 
 			list(const allocator_type &alloc = allocator_type())
 			{
-				Node *tmp = new Node;
 				_allocator = alloc;
 				_length = 0;
-				_capacity = 0;
-//				init(); - check ici -
-				_list.first = tmp;
-				_list.last = tmp;
+				init();
 			}
 
 			list(size_type n, const value_type &val = value_type(), const allocator_type &alloc = allocator_type())
 			{
 				_allocator = alloc;
 				_length = n;
-				_capacity = n;
 				init();
 			}
 
 			~list()
 			{
-				Node *remove;
-				Node *tmp;
+				Node<T> *remove;
+				Node<T> *tmp;
 
 				remove = _list.first;
 				while (remove != NULL)
@@ -133,9 +132,62 @@ namespace ft
 				init();
 			}
 
+			//Iterator
+			iterator begin()
+			{
+				return (iterator(_list.first));
+			}
+
+			iterator end()
+			{
+				if (_length == 0)
+					return (iterator(_list.first));
+				else
+					return (iterator(_list.last->next));
+			}
+
+			//Capacity
+			bool empty() const
+			{
+				return (_length == 0);
+			}
+
+			size_type size() const
+			{
+				return (_length);
+			}
+
+			size_type max_size() const
+			{
+				//to_do;
+				return (0);
+			}
+
+			//Element access
+			reference front()
+			{
+				return (_list.first->c);
+			}
+
+			const_reference front() const
+			{
+				return (_list.first->c);
+			}
+
+			reference back()
+			{
+				return (_list.last->c);
+			}
+
+			const_reference back() const
+			{
+				return (_list.last->c);
+			}
+
+			//Modifiers
 			void push_back(const value_type &val)
 			{
-				Node *node = new Node(val);
+				Node<T> *node = new Node<T>(val);
 				node->next = NULL;
 				node->prev = _list.last;
 
@@ -144,12 +196,12 @@ namespace ft
 				else
 					_list.first = node;
 				_list.last = node;
-
+				_length++;
 			}
 
 			void push_front(const value_type &val)
 			{
-				Node *node = new Node(val);
+				Node<T> *node = new Node<T>(val);
 				node->next = _list.first;
 				node->prev = NULL;
 
@@ -158,17 +210,25 @@ namespace ft
 				else
 					_list.last = node;
 				_list.first = node;
+				_length++;
 			}
 
-			iterator begin()
+			void pop_back()
 			{
-				return (iterator(_list.first));
+				Node<T> *tmp = _list.last->prev;
+				delete _list.last;
+				_list.last = tmp;
+				_length--;
 			}
 
-			iterator end()
+			void pop_front()
 			{
-				return (iterator(_list.last->next));
+				Node<T> *tmp = _list.first->next;
+				delete _list.first;
+				_list.first = tmp;
+				_length--;
 			}
+
 
 	};
 }
--- list/main.cpp
+++ list/main.cpp
@@ -6,37 +6,51 @@ int main()
 
 	std::cout << "\033[1;32mft::list\033[0m\n";
 	{
-		ft::list<int> list;
-		ft::list<int>::iterator it;
+		ft::list<std::string> list;
+		ft::list<std::string>::iterator it;
+		ft::list<std::string>::iterator ite;
 
-		list.push_back(99);
-		list.push_back(66);
+
+		list.push_back("cc");
+		list.push_back("yo");
+
+		list.pop_front();
+		list.pop_back();
+
+		list.push_back("lol");
+
 		it = list.begin();
+		ite = list.end();
 
-//		it++;
+		std::cout << list.size() << std::endl;
 
-
-		std::cout << *it << std::endl;
-
-
+		while (it != ite)
+		{
+			std::cout << *it++ << std::endl;
+		}
 	}
 	std::cout << "\033[1;32mstd::list\033[0m\n";
 	{
-		std::list<int> list;
-		std::list<int>::iterator it;
+		std::list<std::string> list;
+		std::list<std::string>::const_iterator it;
+		std::list<std::string>::const_iterator ite;
 
-//		list.push_back(99);
-//		list.push_back(66);
+		list.push_back("cc");
+		list.push_back("yo");
+
+		list.pop_front();
+		list.pop_back();
+
+		list.push_back("lol");
+
 		it = list.begin();
+		ite = list.end();
 
-//		it++;
+		std::cout << list.size() << std::endl;
 
-//		it[4] = 45;
-//
-//		list[1] = 45;
-
-		std::cout << list.size() << " | ";
-		std::cout << *it << std::endl;
+		while (it != ite)
+		{
+			std::cout << *it++ << std::endl;
+		}
 	}
-
 }
